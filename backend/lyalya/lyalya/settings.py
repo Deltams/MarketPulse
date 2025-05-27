baf180d8ff077ed5057b81da2b9df58ecb0a9c51
Deltams <deltamso@bk.ru>
2025-05-27 22:44:23 +0800

    Добавлен запрет на погружение мусора

--- backend/lyalya/lyalya/settings.py
+++ backend/lyalya/lyalya/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': {
+    'default': { 
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'data_base_market_pulse',
         'USER': 'postgres',
