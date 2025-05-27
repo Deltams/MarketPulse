33ad96d9df7c8519677323d2e8a58642619125d2
Deltams <deltamso@bk.ru>
2025-05-27 21:03:39 +0800

    edit settings and init DB data_base_market_pulse

---
+++ backend/app/config/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE data_base_market_pulse;
+
+GRANT ALL PRIVILEGES ON DATABASE data_base_market_pulse TO postgres;
--- backend/lyalya/lyalya/settings.py
+++ backend/lyalya/lyalya/settings.py
@@ -77,9 +77,18 @@ WSGI_APPLICATION = 'lyalya.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # Для тестов и локального запуска 
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'data_base_market_pulse',
+        'USER': 'postgres',
+        'PASSWORD': 'qwer4617',
+        'HOST': 'my-postgres-2025',
+        'PORT': '5432',
     }
 }
 
