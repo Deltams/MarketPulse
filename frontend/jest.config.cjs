481ad8d27a392d355286c602501dae7f493e7e21
Andrey Severgin <a.severgin@smartdata.dev>
2025-06-16 00:10:27 +0800

    Настроен Jest для фронтенда, добавлены базовые тесты и конфиги

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['vue', 'js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+} 
